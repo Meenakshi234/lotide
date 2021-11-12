8d9a38bb5b7ddb01dc5497c61928262ed9b3341a
Meenakshi234 <virk.meenakshi@gmail.com>
2021-11-12 17:23:54 +0000

    assertEqual Assignment

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual,expected) {
+  let str1 = actual;
+  let str2 = (expected);
+  let result = (str1 === str2);
+  if (result) {
+    console.log('✅✅✅' + "Assertion Passed:[%s] === [%s]",str1,str2);
+  } else {
+    console.log('🛑🛑🛑' + "Assertion Failed:[%s] !== [%s]",str1,str2);
+  }
+  //console.assert(actual.equals(expected) expected,'Assertion Failed:'
+};
+assertEqual("Lighthouse Labs","Bootcamp");
+assertEqual(1,1);
+assertEqual("Lighthouse Labs","Lighthouse Labs");
+assertEqual(1,23);
