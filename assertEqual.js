42333f56d4a6a238cbd84197862b44991802c606
Meenakshi234 <virk.meenakshi@gmail.com>
2021-11-27 11:13:05 -0500

    Exporting the asserEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,14 @@
 const assertEqual = function(actual,expected) {
   let str1 = actual;
-  let str2 = (expected);
+  let str2 = expected;
   let result = (str1 === str2);
   if (result) {
-    console.log('✅✅✅' + "Assertion Passed:[%s] === [%s]",str1,str2);
+   return console.log('✅✅✅' + "Assertion Passed:[%s] === [%s]",str1,str2);
   } else {
-    console.log('🛑🛑🛑' + "Assertion Failed:[%s] !== [%s]",str1,str2);
+    return console.log('🛑🛑🛑' + "Assertion Failed:[%s] !== [%s]",str1,str2);
   }
 };
-assertEqual("Lighthouse Labs","Bootcamp");
-assertEqual(1,1);
-assertEqual("Lighthouse Labs","Lighthouse Labs");
-assertEqual(1,23);
+
+
+//  export the function
+module.exports = assertEqual;
