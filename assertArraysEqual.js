b26865e3cca8f54ac8c84d1e6512add20cd82d92
Meenakshi234 <virk.meenakshi@gmail.com>
2021-11-27 11:52:47 -0500

    exporting assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,37 +1,16 @@
-const eqArrays =(a,b) =>{
-    //1. Check if the two values are identical.
-    if(a === b) return true;
+const eqArrays =require('./eqArrays');
 
-    //2. Check if both values are Date objects with the same time, using Date.prototype.getTime().
-    if (a instanceof Date && b instanceof Date)
-      return a.getTime() === b.getTime();
-
-   //3. Check if both values are non-object values with an equivalent value (strict comparison).
-    if (!a || !b || (typeof a !== 'object' && typeof b !== 'object'))
-    return a === b;
-
-   //4. Check if only one value is null or undefined or if their prototypes differ.
-    if(a.prototype !== b.prototype) return false;
-
-   //5. If none of the above conditions are met, use Object.keys() to check if both values have the same number of keys.
-  const keys =Object.keys(a);
-  if(keys.length !== Object.keys(b).length) return false;
-
-   //6. Use Array.prototype.every() to check if every key in a exists in b and if they are equivalent by calling eqArrays() recursively.
-  return keys.every(k => eqArrays(a[k], b[k]));
-};
-
-const assertEqual = function(actual,expected) {
+/* const assertEqual = function(actual,expected) {
     let str1 = actual;
-    let str2 = (expected);
+    let str2 = expected;
     let result = (str1 === str2);
     if (result) {
       console.log('✅✅✅' + "Assertion Passed:[%s] === [%s]",str1,str2);
     } else {
       console.log('🛑🛑🛑' + "Assertion Failed:[%s] !== [%s]",str1,str2);
     }
-  };
-function assertArraysEqual(array1,array2)
+  }; */
+const assertArraysEquals = function(array1,array2)
 {
     let result = eqArrays(array1,array2);
     if (result) {
@@ -40,8 +19,5 @@ function assertArraysEqual(array1,array2)
         console.log('🛑🛑🛑' + "Assertion Failed:",array1,array2);
       }
     
-}
-assertArraysEquals([1, 2, 3], [1, 2, 3]);
-assertArraysEquals([1,2,4,5],[1,3,6,7]);
-assertArraysEquals(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEquals(["1", "2", "3"], ["1", "2", 3]);
+};
+module.exports = assertArraysEquals;
